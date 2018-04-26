a6ed4552b77e22a62519824d968dda3328c96f08
victorreis <v.crisostomo.reis@gmail.com>
2018-04-26 13:27:00 -0300

    Exemplos de comandos adicionados

--- R/file_manager.R
+++ R/file_manager.R
@@ -11,3 +11,28 @@ leDados <- function(){
 leIndicadores <- function(){
     indicadores <- read.csv("~/Documentos/Programs/R Language/World Bank/data/Metadata_Indicator_API_Download_DS2_en_csv_v2_9444809.csv")
 }
+
+
+# Para saber a estrutura de PAISES
+#str(paises)
+
+# PAISES é um frame?
+#print(is.data.frame(paises))
+
+# número de colunas de PAISES
+#print(ncol(paises))
+
+# número de linhas de PAISES
+#print(nrow(paises))
+
+# EXEMPLO: Extract Specific columns.
+#result <- data.frame(paises$"ï..Country.Code")
+#result
+
+# Analisando os investimentos em educação (SE.XPD.TOTL.GD.ZS)
+#investPorPais <- subset(dados, Indicator.Code == "SE.XPD.TOTL.GD.ZS", select = c(Country.Code), DROP=FALSE)
+#investPorPais
+
+# max(investPorPais$x2011,na.rm=TRUE)
+# retval <- subset(investPorPais, x2011 == max(double(x2011)))
+# retval
